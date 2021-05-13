bf746baf8b96ad19e3c40f7954f44549d53a2082
ZivFerdinand <71594716+ZivFerdinand@users.noreply.github.com>
2021-05-13 19:14:03 +0700

    Create Hexadecimal's_Theorem.cpp

---
+++ Hexadecimal's_Theorem.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#define ll long long
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	cout << "0 0 " << n;
+}
+/*
+* 
+*/
