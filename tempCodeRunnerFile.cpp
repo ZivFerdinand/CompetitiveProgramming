cc6f2ad5ebcea6c22e1b6c333cf4167da37733cf
ZivFerdinand <zivenf@gmail.com>
2021-09-11 15:28:03 +0700

    VSC

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,29 @@
-10 3 5 1
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+using namespace std;
+long long patok[100010];
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < n;i++)
+    {
+        int x;
+        cin >> x;
+
+        if(i>0)
+        {
+            patok[i] = patok[i - 1] + x;
+        }
+        else
+            patok[i] = x;
+    }
+    int x;
+    while(k--)
+    {
+        cin >> x;
+        cout << 1+ upper_bound(patok, patok + n, x) << endl;
+    }
+}
