be8e65139769d408369d4a3dabbbbc3faf5f50da
ZivFerdinand <zivenf@gmail.com>
2021-09-11 15:28:23 +0700

    Create Pembatasan_Dunia.cpp

---
+++ Pembatasan_Dunia.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <sstream>
+#include <vector>
+#include <deque>
+#define ll long long
+using namespace std;
+
+int main(){
+    int a, x;
+    cin >> a >> x;
+
+    cout << x / a << ' ' << x % a << endl;
+}
+/*
+
+
+*/
