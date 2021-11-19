d8e7f0caeb8c1a536946a65a814f790fd7b13c46
ZivFerdinand <zivenf@gmail.com>
2021-11-19 13:10:18 +0700

    Update buatBuat.cpp

--- buatBuat.cpp
+++ buatBuat.cpp
@@ -13,45 +13,36 @@
 using namespace std;
 
 int main(){
-    int q;
-    cin >> q;
-
-    ll n, h, x;
-    ll max1, max2;
-    while(q--)
+    int n = 9, x;
+    ll cnt;
+    while(cin >> x)
     {
-        cin >> n >> h;
-        max1 = max2 = 0;
-        for (int i = 0; i < n;i++)
+        cnt = 0;
+        if(x==0 && n==0)
         {
-            cin >> x;
-            if(x>max1)
-            {
-                max2 = max1;
-                max1 = x;
-            }
-            else if(x>=max2)
-                max2 = x;
-        }
-
-        if(h-max1 < 0)
-        {
-            cout << 1 << '\n';
             continue;
         }
-        else
-        {
-            int ans = h / (max2 + max1);
-            if(h%(max2+max1))
-                ans++;
 
-            int xx = 0;
-            if(h-(ans*(max1+max2)) + max2 <= 0)
-                xx=1;
-            cout << ans*2-xx<< '\n';
+        int i, j, k, l;
+        for (i = 0; i <= n/* && i + j + k <= x*/; i++)
+        {
+            for (j = 0; j <= n/* && i + j + k <= x*/;j++)
+            {
+                for (k = 0; k <= n/* && i + j + k <= x*/;k++)
+                {
+                    for (l = 0; l <= n/* && i + j + k <= x*/;l++)
+                    {
+                        if(i+j+k+l==x)
+                            cnt++;
+                    }
+                }
+            }
         }
+
+        cout << cnt << '\n';
     }
 }
 /*
-
+0 1 2 3 4   = 5
+0 1 2 3 4 5 = 6
 */
