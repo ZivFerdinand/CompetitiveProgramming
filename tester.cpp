23ccc9ae1cfad85c84fa1f39a385a28223c5244e
ZivFerdinand <zivenf@gmail.com>
2021-11-22 16:16:39 +0700

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -1,13 +1,58 @@
-#include<cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <cstring>
+#include <iomanip>
+#include <map>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <deque>
+#define ll long long
 using namespace std;
-int main(){
- int a, b, keliling, luas;
- printf("Panjang : ");
- scanf("%d", &a);
- printf("Lebar : ");
- scanf("%d", &b);
- keliling = 2*(a+b);
- luas = a*b;
- printf("Keliling = %d\n", keliling);
- printf("Luas = %d\n", luas); 
+
+int main()
+{
+    int n, originalVal;
+    cin >> n;
+    originalVal = n;
+
+    int space = 0;
+    string temp;
+
+    //n *= 2;
+    while(n > 0)
+    {
+
+        //temp = "";
+        // Create Spacet
+        temp = "";
+        for (int i = 0; i < (originalVal - n - space) / n;i++)
+        {
+            temp += " ";
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << temp << '*';
+        }
+
+        cout << '\n';
+        n /= 2;
+
+        space *= 2;
+        space++;
+    }
 }
+/*
+***************
+ * * * * * * *
+   *   *   *
+       *
+        
+       
+********
+ * * * 
+*/
